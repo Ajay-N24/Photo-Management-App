bd89a83559319e76a9b5cbe2d4655b13eca1b5ca
unknown <ajayprmk@gmail.com>
2024-06-04 21:49:26 +0530

    Added Search Functionality

--- Client/src/screens/Gallery.jsx
+++ Client/src/screens/Gallery.jsx
@@ -16,8 +16,10 @@ const Gallery = () => {
   let [pd, setPd] = useState([]);
   let [index, setIndex] = useState(-1);
   const [open, setOpen] = React.useState(false);
+  const [searchText, setSearchText] = useState();
+  const [searchResult, setsearchResult] = useState();
   let photoD = useSelector((state) => state.photos.value);
-  console.log(photoD);
+  // console.log(photoD);
 
   useEffect(() => {
     async function fetchData() {
@@ -45,6 +47,18 @@ const Gallery = () => {
     setIndex(count);
     setOpen(true);
   };
+  const handleEnter = () => {
+    const titleSearch = pd.filter(
+      (data) => data.title.toLowerCase() == searchText.toLowerCase()
+    );
+    console.log(titleSearch);
+    setsearchResult(titleSearch);
+  };
+  useEffect(() => {
+    if (searchText === "" || undefined) {
+      setsearchResult();
+    }
+  }, [searchText]);
   return (
     <div>
       <Navbar />
@@ -53,7 +67,55 @@ const Gallery = () => {
           Open Lightbox
         </button> */}
         {console.log(photoD)}
-        <Photos data={pd} onHandle={handlePhotoClick} />
+        <div className="flex mt-10 relative items-center mx-20">
+          <input
+            type="text"
+            className="w-full h-10 border-2 border-black rounded-xl pl-6 pr-14 text-lg"
+            placeholder="Search for Photos"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") handleEnter();
+            }}
+          />
+          <div
+            className="absolute z-10 ml-[95.4%] max-lg:ml-[93.6%] max-md:ml-[93%] max-sm:ml-[89%] cursor-pointer"
+            onClick={handleEnter}
+          >
+            <svg
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#000000"
+            >
+              <path
+                d="M17 17L21 21"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+              <path
+                d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z"
+                stroke="#000000"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        {console.log(searchText)}
+        {console.log(searchResult)}
+        <Photos
+          data={pd}
+          onHandle={handlePhotoClick}
+          searchData={searchResult}
+        />
         <Lightbox
           plugins={[Captions, Download, Fullscreen, Zoom, Thumbnails]}
           open={open}
--- Client/src/screens/Photos.jsx
+++ Client/src/screens/Photos.jsx
@@ -1,24 +1,42 @@
 import React, { useEffect, useState } from "react";
-const Photos = ({ data, onHandle }) => {
+const Photos = ({ data, onHandle, searchData }) => {
   return (
     <div className="grid grid-cols-3 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-2 gap-5 mt-16 mb-10">
-      {data.map((data, index) => {
-        return (
-          <div
-            key={index}
-            className="relative cursor-pointer"
-            onClick={() => {
-              onHandle(index);
-            }}
-          >
-            <div className="absolute pl-3 text-white">{data.title}</div>
-            <img src={data.url} alt="" className=" max-w-full rounded-lg" />
-            <div className="absolute pl-3 mt-[-30px] text-white">
-              {data.description}
-            </div>
-          </div>
-        );
-      })}
+      {searchData
+        ? searchData.map((data, index) => {
+            return (
+              <div
+                key={index}
+                className="relative cursor-pointer"
+                onClick={() => {
+                  onHandle(index);
+                }}
+              >
+                <div className="absolute pl-3 text-white">{data.title}</div>
+                <img src={data.url} alt="" className=" max-w-full rounded-lg" />
+                <div className="absolute pl-3 mt-[-30px] text-white">
+                  {data.description}
+                </div>
+              </div>
+            );
+          })
+        : data.map((data, index) => {
+            return (
+              <div
+                key={index}
+                className="relative cursor-pointer"
+                onClick={() => {
+                  onHandle(index);
+                }}
+              >
+                <div className="absolute pl-3 text-white">{data.title}</div>
+                <img src={data.url} alt="" className=" max-w-full rounded-lg" />
+                <div className="absolute pl-3 mt-[-30px] text-white">
+                  {data.description}
+                </div>
+              </div>
+            );
+          })}
     </div>
   );
 };
